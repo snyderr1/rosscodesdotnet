a6ee5786655dba2bd06eaa9a7fd97bcacd94e70d
Ross Snyder <snyderr@oregonstate.edu>
2023-12-13 19:28:09 -0800

    linting!

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link';
 function NavBar(props: any) {
 	return (
 		<div className='w-full h-8 m-2 flex justify-center items-center bg-slate-950 opacity-95 mt-0 pt-1'>
-			
 			<ul className='h-full w-[1620px] flex justify-center items-center space-x-10  text-white'>
 				<li className='hover:bg-black hover:bg-opacity-35' key='rosscodes'>
 					<Link href='https://rosscodes.net/' className='font-medium text-lg hover:underline text-white'>RossCodes.net</Link>
