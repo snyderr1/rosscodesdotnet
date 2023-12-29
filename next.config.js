93d1c221de92527ff3c13423b171217df960a5c0
Ross Snyder <rossesny@gmail.com>
2023-12-28 19:50:14 -0800

    imag dbugging

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	output: 'export',
 	images: {
 		unoptimized: true,
-		domains: ['i.imgur.com'],
+		domains: ['i.imgur.com', 'imgur.com', 'https://imgur.com', 'https://i.imgur.com'],
 	},
 };
 
